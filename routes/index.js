f3bdeee5793291826d83956663f8a599a3900805
Sebastien Morelle <smorel14@gmail.com>
2019-07-09 09:11:37 +0100

    seb

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,48 @@
 const express = require('express')
 const Article = require('../models/Article')
+const uploadCload = require('../congfig/cloudinary')
 const router  = express.Router()
 
 
 router.get('/', (req, res, next) => {
-  console.log('TEST')
-
   Article.find()
     .then(articlesFromDb => {
-      console.log('articlesFromDb', articlesFromDb)
       res.render('index', {
         articles: articlesFromDb
       })
     })
 })
 
+router.get('/addArticle', (req, res, next) => {
+  console.log('we are here');
+  res.render('addArticle')
+})
 
-router.get('/:articlesId', (req, res, next) => {
-  let articlesId = req.params.articlesId
-  Article.findById(articlesId)
-    .then(articlesFromDb => {
-      res.render('index', {
-        articles: articlesFromDb
-      })
-    })
+router.post('/addArticle', uploadCload.single('photo'), (req,res,next)=>{
+  console.log("Wohoooo")
+  const{title, description, link, date, votingDate} = req.body;
+  const imgArticle = req.file.url;
+  Article.create(
+    {title, description, link, date, votingDate, imgArticle}
+  ).then(articles => {
+    res.redirect('/');
+  }).catch( error => {
+    console.log(error);
+  })
 })
 
 module.exports = router
+
+
+
+
+
+// router.get('/article/:articlesId', (req, res, next) => {
+//   let articlesId = req.params.articlesId
+//   Article.findById(articlesId)
+//     .then(articlesFromDb => {
+//       res.render('index', {
+//         articles: articlesFromDb
+//       })
+//     })
+// })
