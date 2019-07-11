47dc22f64e51b60ee669530ce673a560d2071480
António Mesquita e Ferreira <antonio@Antonios-MacBook-Pro.local>
2019-07-11 19:49:26 +0100

    a

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Seeds file that remove all users and create 2 new users
 
@@ -27,21 +27,19 @@ mongoose
 let users = [
   {
     password: bcrypt.hashSync("alice", bcrypt.genSaltSync(bcryptSalt)),
-    name: 'alice',
+    name: "alice",
     party: "Labour",
-    politicalView: 'Undecided',
+    politicalView: "Undecided",
     role: "user",
     status: "confirmed"
-
-
-
   },
   {
     email: "bob",
+    profilePicture: "/images/bob1.jpg",
     password: bcrypt.hashSync("bob", bcrypt.genSaltSync(bcryptSalt)),
     name: "bob",
     party: "Labour",
-    politicalView: 'Changing the world',
+    politicalView: "Changing the world",
     role: "admin",
     status: "confirmed"
   },
@@ -52,14 +50,45 @@ let users = [
     password: bcrypt.hashSync("boris", bcrypt.genSaltSync(bcryptSalt)),
     name: "Boris Johnson",
     party: "Conservative",
-    politicalView: 'Voting Tory will cause your wife to have bigger breasts and increase your chances of owning a BMW M3',
+    politicalView:
+      "Voting Tory will cause your wife to have bigger breasts and increase your chances of owning a BMW M3",
     role: "admin",
     status: "confirmed"
+  },
 
+  {
+    email: "theresa",
+    profilePicture: "/images/download.theresa.jpeg",
+    password: bcrypt.hashSync("theresa", bcrypt.genSaltSync(bcryptSalt)),
+    name: "Theresa May",
+    party: "Conservative",
+    politicalView: "I do not know what I am doing",
+    role: "admin",
+    status: "confirmed"
+  },
+
+  {
+    email: "corbyn",
+    profilePicture: "/images/jeremy-corbyn.jpg",
+    password: bcrypt.hashSync("corbyn", bcrypt.genSaltSync(bcryptSalt)),
+    name: "Jeremy Corbyn",
+    party: "Labour",
+    politicalView: "Vote labour and I will give you a lot of money!",
+    role: "admin",
+    status: "confirmed"
+  },
+
+  {
+    email: "bobbuilder",
+    profilePicture: "/images/bob.jpeg",
+    password: bcrypt.hashSync("bobbuilder", bcrypt.genSaltSync(bcryptSalt)),
+    name: "Bob The Builder",
+    party: "Conservative",
+    politicalView: "Get back to work",
+    role: "admin",
+    status: "confirmed"
   }
-
-
-]
+];
 
 User.deleteMany()
   .then(() => {
@@ -81,21 +110,34 @@ User.deleteMany()
 //articles
 let articles = [
   {
-    title: "Brexit: All you need to know about the UK leaving the EU",
+    title:
+      "Are you for or against Brexit? All you need to know about the UK leaving the EU",
     description:
       "Here is an easy-to-understand guide to Brexit - beginning with the basics, then a look at the current negotiations, followed by a selection of answers to questions we've been sent.",
-    imgArticle: "https://ichef.bbci.co.uk/news/660/cpsprodpb/2847/production/_90911301_c5b48218-424a-41c4-9b6b-638600ec508f.jpg",
+    imgArticle:
+      "https://ichef.bbci.co.uk/news/660/cpsprodpb/2847/production/_90911301_c5b48218-424a-41c4-9b6b-638600ec508f.jpg",
     link: "https://www.bbc.com/news/uk-politics-32810887",
     date: "08-07-2019",
-    votingDate: 03/03/2019
+    votingDate: 03 / 09 / 2019
   },
   {
     title: "No deal Brexit",
-    description: String,
-    img: String,
+    description: "what do you think?",
+    imgArticle:
+      "https://g8fip1kplyr33r3krz5b97d1-wpengine.netdna-ssl.com/wp-content/uploads/2018/03/h_54159221-1160x753.jpg",
     link: String,
-    date: String,
-    votingDate: String
+    date: 10 / 07 / 2019,
+    votingDate: 10 / 10 / 2019
+  },
+  {
+    title: "Do you want Boris as PM?",
+    description:
+      "Boris Johnson looks to have won the Conservative leadership contest by a landslide, 12 days before the race is over. According to a poll of 1,319 party members by ConservativeHome,  71 per cent said they had already voted, with 72 per cent backing Mr Johnson and 28 per cent, Jeremy Hunt.",
+    imgArticle: "https://cdn.britannica.com/59/143959-004-99295B62.jpg",
+    link:
+      "https://www.telegraph.co.uk/politics/2019/07/11/tory-leadership-boris-johnson-sees-trump-lifeboat-rescue-brexit/",
+    date: 10 / 07 / 2019,
+    votingDate: 20 / 10 / 2019
   }
 ];
 
@@ -107,6 +149,7 @@ Article.deleteMany()
     console.log(
       `${articlesCreated.length} article created with the following id:`
     );
+    console.log(articlesCreated.map(u => u._id));
   })
   .then(() => {
     // Close properly the connection to Mongoose
@@ -117,26 +160,36 @@ Article.deleteMany()
     throw err;
   });
 
+let votes = [
+  {
+    _owner: "5d2338109214330a21c45e17",
+    _article: "5d2338109214330a21c45e1a",
+    option: "Supporting",
+    visible: "visible"
+  },
 
-  let votes = [
-    {
-      _ownerID : "5d2338109214330a21c45e17",
-      _articleID : "5d2338109214330a21c45e1a",
-      option: 'for',
-      visible: 'visible'
-    
-    }
-  
-  ];
+  {
+    _owner: "5d275e5b2b1b8a26940c7f0e",
+    _article: "5d275fbdf61aa52702dba0df",
+    option: "Supporting",
+    visible: "visible"
+  },
 
-  Votes.deleteMany()
+  {
+    _owner: "5d276207c80ac427bd8dd7d9",
+    _article: "5d275fbdf61aa52702dba0df",
+    option: "Not-Supporting",
+    visible: "visible"
+  }
+];
+
+Votes.deleteMany()
   .then(() => {
     return Votes.create(votes);
   })
   .then(votesCreated => {
-    console.log(
-      `${votesCreated.length} vote created with the following id:`
-    );
+    console.log(`${votesCreated.length} vote created with the following id:`);
+    console.log(votesCreated.map(u => u._id));
   })
   .then(() => {
     // Close properly the connection to Mongoose
--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,12 @@ const { checkAdmin, checkUser } = require("../middlewares/middlewares");
 
 router.get("/", (req, res, next) => {
   Article.find().then(articles => {
-   let yes = articles[0].voteYes;
-   console.log(yes)
+    let yes = articles[0].voteYes;
+    console.log(yes);
     res.render("index", {
       articles: articles,
-      title: "News",
-     // percentageVotes: 
+      title: "News"
+      // percentageVotes:
     });
   });
 });
@@ -89,16 +89,16 @@ router.get("/voting/:articlesId", (req, res, next) => {
 router.get("/profile/:userId", (req, res, next) => {
   let userId = req.params.userId;
   //User.findById(userId).then(user => {
-    Vote.find({_owner : userId})
+  Vote.find({ _owner: userId })
     .populate("_owner")
     .populate("_article")
-    .then((vote)=>{
-      console.log('vote', vote)
+    .then(vote => {
+      console.log("vote", vote);
       res.render("profile", {
         vote: vote
       });
-    })
-//  });
+    });
+  //  });
 });
 
 // router.get('/profile/:userId', checkUser,(req, res, next) => {
@@ -120,248 +120,178 @@ router.get("/voting/:articlesId", checkUser, (req, res, next) => {
   });
 });
 
-
-
 router.post("/voting/:articlesId", (req, res, next) => {
   let _owner = req.user.id;
   let option = req.body.select1;
   let visible = req.body.select2;
   let _article = req.params.articlesId;
-  console.log('option', option, _owner, visible, _article);
-  if (option && visible){
+  console.log("option", option, _owner, visible, _article);
+  if (option && visible) {
     Article.findById(_article)
-    .then( (article) =>{
-      for(let i = 0; i < article.voteYes.length; i++){
-        console.log('check if the same', article.voteYes[i], req.user.id)
-        if(article.voteYes[i].toString() === req.user.id.toString()){
-        console.log('check if the same', article.voteYes[i], req.user.id)
-          res.render("alreadyVoted");
-          return;
+      .then(article => {
+        for (let i = 0; i < article.voteYes.length; i++) {
+          console.log("check if the same", article.voteYes[i], req.user.id);
+          if (article.voteYes[i].toString() === req.user.id.toString()) {
+            console.log("check if the same", article.voteYes[i], req.user.id);
+            res.render("alreadyVoted");
+            return;
+          }
         }
-
-      }
-      if(option === 'Supporting'){
-        console.log('article', article.voteNo, option, req.user.id)
-        article.voteYes.push(req.user.id);
-        article.save()
-      }
-      else{
-        article.voteNo.push(req.user.id);
-        article.save()
-      }
-      Vote.create({ _owner, _article, option, visible })
-      .then(() => {
-        res.redirect("/");
-      });      
-    })
-    .catch(err =>{
-      res.render("auth/login", { message: "You have an error" });
-    })
-  }
-  else{
+        if (option === "Supporting") {
+          console.log("article", article.voteNo, option, req.user.id);
+          article.voteYes.push(req.user.id);
+          article.save();
+        } else {
+          article.voteNo.push(req.user.id);
+          article.save();
+        }
+        Vote.create({ _owner, _article, option, visible }).then(() => {
+          res.redirect("/");
+        });
+      })
+      .catch(err => {
+        res.render("auth/login", { message: "You have an error" });
+      });
+  } else {
     res.render("voting", { message: "please fill out the ID" });
   }
-  });
+});
 
 
-
-// router.post('/voting', (req,res,next)=>{
-//   console.log("working?")
-
-//   .then( => {
-//     res.redirect('/');
-//   }).catch( error => {
-//     console.log(error);
-//   })
-// });
-
-// router.post('/profile', (req, res, next) => {
-//   let userId = req.params.userId
-//   let imgUser = req.user.profilePicture
-//   User.findById(userId)
-//     .then(userFromDb => {
-//       res.render('profile', {
-//         user : userFromDb,
-//         // profilePicture: imgUser,
-//       })
-//     })
-// })
 
 router.get("/profileList", (req, res, next) => {
-  
-  res.render("profile-list");
+  User.find().then(userFromDb => {
+    console.log("user coming", userFromDb);
+    res.render("profile-list", {
+      user: userFromDb
+      // userId: userId
+    });
+  });
 });
 
 router.post("/profileList", (req, res, next) => {
-    let userId = req.params.userId;
-    let name = req.body.name
-    let searchName = new RegExp(`${name}`, "i")
-    console.log("name is", name)
-    User.find({name: searchName})
-      .then(userFromDb => {
-        console.log("user coming", userFromDb)
-      res.render('profile-list', {
-        user: userFromDb,
-        userId: userId
+  //let userId = req.params.userId;
+  let name = req.body.name;
+  let searchName = new RegExp(`${name}`, "i");
+  console.log("name is", name);
+  User.find({ name: searchName }).then(userFromDb => {
+    console.log("user coming", userFromDb);
+    res.render("profile-list", {
+      user: userFromDb
+      // userId: userId
     });
-  })
+  });
 });
-
-//User.find({name: name})
-
-
-  // let party = req.body.party
-  //   User.find({party: party})
-  //     .then(partyFromDb =>{
-  //       res.render('profile-list', {
-  //       party : partyFromDb  
-
-  //     })
-  //   })
-    
-
-// router.post("/profileList", (req, res, next) => {
-//   let userId = req.params.userId
-
-//   User.find({
-//     $text: {
-//       $search: userId
-//     }
-//   }, {
-//     _id: 0,
-//   }, function(err, data){
-//     res.json(data);
-    
-//   }).limit(4);
-// });
 
 
 
 module.exports = router;
 
+// router.get('/', (req, res, next) => {
+//   let totalVotesList = [];
+//   Article.find()
+//     .then(articles => {
+//       articles.map( article => {
+//         Vote.find({_articleID : article.id})
+//         .then(votes => {
+//           //console.log('article', article)
+//          // console.log('vote', votes.length)
+//           totalVotesList.push([article.id, votes.length])
+//         })
+//       })
+//       console.log('totalVotesList', totalVotesList)
+//       //console.log('article2', articles)
+//       })
+//       res.render('index', {
+//         totalVotesList: totalVotesList,
+//         title: "News"
+//     })
+//     })
 
+// router.get('/', (req, res, next) => {
+//   let totalVotesList = [];
+//   let newidea = [];
+//   Article.find()
+//     .then(articles => {
+//       articles.map( article => {
+//         Vote.find({_articleID : article.id})
+//         .then(votes => {
+//           //console.log('article', article)
+//          // console.log('vote', votes.length)
+//           totalVotesList.push([article.id, votes.length])
+//         })
+//         .then(() => {
+//          console.log(totalVotesList)
+//         })
+//       })
+//       //console.log('totalVotesList', newidea)
+//       //console.log('article2', articles)
+//       })
+//       res.render('index', {
+//         totalVotesList: totalVotesList,
+//         title: "News"
+//     })
+//     })
 
+// router.get('/', (req, res, next) => {
+//   let totalVotesList = [];
+//   Article.find()
+//     .then(articles => {
+//       for(let i = 0; i < articles.length; i++){
+//         Vote.find({_articleID : articles[i].id})
+//         .then(votes => {
+//           //console.log('article', article)
+//          // console.log('vote', votes.length)
+//          console.log(i, 'totalVotesList', totalVotesList)
+//          totalVotesList = totalVotesList.push([articles[i].id, votes.length])
+//         })
+//       }
+//       console.log('totalVotesList', totalVotesList)
+//       //console.log('article2', articles)
+//       })
+//       res.render('index', {
+//         totalVotesList: totalVotesList,
+//         title: "News"
+//     })
+//     })
 
+// router.get('/', (req, res, next) => {
+//   Vote.find()
+//   .populate("_articleID")
+//   .then (votes => {
 
+//   })
 
+// })
 
-  // router.get('/', (req, res, next) => {
-  //   let totalVotesList = [];
-  //   Article.find()
-  //     .then(articles => {
-  //       articles.map( article => {
-  //         Vote.find({_articleID : article.id})
-  //         .then(votes => {
-  //           //console.log('article', article)
-  //          // console.log('vote', votes.length)
-  //           totalVotesList.push([article.id, votes.length])
-  //         })
-  //       })
-  //       console.log('totalVotesList', totalVotesList)
-  //       //console.log('article2', articles)
-  //       })
-  //       res.render('index', {
-  //         totalVotesList: totalVotesList,
-  //         title: "News"
-  //     })
-  //     })
+//   router.get('/', (req, res, next) => {
+//     Promise.all([
+//       Article.find(),
+//       Vote.find({_articleID : articles[i].id})
+//     ]).then((responses)=>{
+//       res.render('index', {
+//         responses: responses,
+//         title: "News"
+//     })
+//   })
+// })
 
-
-
-  // router.get('/', (req, res, next) => {
-  //   let totalVotesList = [];
-  //   let newidea = [];
-  //   Article.find()
-  //     .then(articles => {
-  //       articles.map( article => {
-  //         Vote.find({_articleID : article.id})
-  //         .then(votes => {
-  //           //console.log('article', article)
-  //          // console.log('vote', votes.length)
-  //           totalVotesList.push([article.id, votes.length])
-  //         })
-  //         .then(() => {
-  //          console.log(totalVotesList)
-  //         })
-  //       })
-  //       //console.log('totalVotesList', newidea)
-  //       //console.log('article2', articles)
-  //       })
-  //       res.render('index', {
-  //         totalVotesList: totalVotesList,
-  //         title: "News"
-  //     })
-  //     })
-
-
-
-
-      // router.get('/', (req, res, next) => {
-      //   let totalVotesList = [];
-      //   Article.find()
-      //     .then(articles => {
-      //       for(let i = 0; i < articles.length; i++){
-      //         Vote.find({_articleID : articles[i].id})
-      //         .then(votes => {
-      //           //console.log('article', article)
-      //          // console.log('vote', votes.length)
-      //          console.log(i, 'totalVotesList', totalVotesList)
-      //          totalVotesList = totalVotesList.push([articles[i].id, votes.length])
-      //         })
-      //       }
-      //       console.log('totalVotesList', totalVotesList)
-      //       //console.log('article2', articles)
-      //       })
-      //       res.render('index', {
-      //         totalVotesList: totalVotesList,
-      //         title: "News"
-      //     })
-      //     })
-
-      // router.get('/', (req, res, next) => {
-      //   Vote.find()
-      //   .populate("_articleID")
-      //   .then (votes => {
-          
-      //   })
-        
-      // })
-     
-  
-
-
-        //   router.get('/', (req, res, next) => {
-        //     Promise.all([
-        //       Article.find(),
-        //       Vote.find({_articleID : articles[i].id})
-        //     ]).then((responses)=>{
-        //       res.render('index', {
-        //         responses: responses,
-        //         title: "News"
-        //     })
-        //   })
-        // })
-
-
-
-
-  // router.get('/', (req, res, next) => {
-  //   let totalVotesList = [];
-  //   Article.find()
-  //     .then(articles => {
-  //       for(let i = 0; i < articles.length; i++){
-  //         Vote.find({_articleID : articles[i].id})
-  //         .then(votes => {
-  //           console.log('votes', votes[0])
-  //           res.render('index', {
-  //             articles: articles,
-  //             title: "News",
-  //         })
-  //     })
-  //   }
-  //   })
-  // })
-
+// router.get('/', (req, res, next) => {
+//   let totalVotesList = [];
+//   Article.find()
+//     .then(articles => {
+//       for(let i = 0; i < articles.length; i++){
+//         Vote.find({_articleID : articles[i].id})
+//         .then(votes => {
+//           console.log('votes', votes[0])
+//           res.render('index', {
+//             articles: articles,
+//             title: "News",
+//         })
+//     })
+//   }
+//   })
+// })
 
 //     router.get('/', (req, res, next) => {
 //       let listVotes
